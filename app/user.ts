3a94be10e1c8a66cadcdc9df79c5f6cb59d99a45
karupotula <madhu.karupotula@valuemomentum.biz>
2016-05-05 15:15:16 +0530

    Create user.ts

---
+++ app/user.ts
@@ -0,0 +1,13 @@
+
+
+export class User {
+    name: string = '';
+    displayname: string = '';
+    email: string = '';
+    password: string = '';
+    address1: string;
+    address2: string;
+    state: string;
+    
+
+}
