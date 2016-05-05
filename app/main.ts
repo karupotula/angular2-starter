8728e69c8a686f362d49cead9faa8bc9191f79a6
karupotula <madhu.karupotula@valuemomentum.biz>
2016-05-05 15:09:21 +0530

    Create main.ts

---
+++ app/main.ts
@@ -0,0 +1,22 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {loginComponent} from './login.component'
+import {HTTP_PROVIDERS} from 'angular2/http';
+debugger;
+bootstrap(loginComponent, [HTTP_PROVIDERS]);
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/Enter file contents heredebugger;
+import {bootstrap} from 'angular2/platform/browser';
+import {loginComponent} from './login.component'
+import {HTTP_PROVIDERS} from 'angular2/http';
+debugger;
+bootstrap(loginComponent, [HTTP_PROVIDERS]);
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
